da9471cae4e8df2bdc6ba828244ed73de636af00
rohan28107 <rohangajre21@gmail.com>
2023-01-09 09:37:38 +0530

    edited yml files

--- next.config.js
+++ next.config.js
@@ -14,31 +14,7 @@ const nextConfig = {
 
     ]
   }
-  // reactStrictMode: true,
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: '.githubusercontent.com',
-  //       port: '',
-  //       pathname: '/u/**',
-  //     },
-  //   ],
-  // },
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: '**.githubusercontent.com',
-  //     },
-  //   ],
-  // },
-  // images: {
-  //   domains: ['avatars.githubusercontent.com'],
-  // },
-  // images: {
-  //   unoptimized: true,
-  // },
+ 
 }
 
 module.exports = nextConfig;
